288bf026154107b827becda4d9ee094281b8580b
Matthew <mray@zoll.com>
2023-09-23 21:51:59 -0400

    feat: player move anim, improved user movement feedback, etc.

---
+++ src/hooks/useEnvironmentObject.jsx
@@ -0,0 +1,44 @@
+ import wallTorchSpriteSheet from "../assets/images/torch-Sheet.png";
+
+ export const useEnvironmentObject = (cellSize, tileSize) => {
+
+  const torch = {
+    sprite: wallTorchSpriteSheet,
+    animation: {
+      frameTotal: 7,
+      frame: 0,
+      speed: 125,
+      lastFrameTime: 0
+    }
+  };
+
+  const renderTorch = (ctx, timestamp, xPosition, yPosition) => {
+    const wallTorch = new Image();
+    wallTorch.src = torch.sprite;
+
+    const renderObjects = (timestamp) => {
+      const deltaTime = timestamp - torch.animation.lastFrameTime;
+      if (deltaTime >= torch.animation.speed) {
+        torch.animation.frame = (torch.animation.frame + 1) % torch.animation.frameTotal;
+        torch.animation.lastFrameTime = timestamp;
+      }
+      return {x: torch.animation.frame * tileSize, y: 0};
+    };
+
+    const {x, y} = renderObjects(timestamp);
+
+    ctx.drawImage(
+      wallTorch, 
+      x, 
+      y, 
+      tileSize, 
+      tileSize, 
+      xPosition, 
+      yPosition, 
+      cellSize,
+      cellSize
+    );
+  };
+
+  return { renderTorch }
+};
